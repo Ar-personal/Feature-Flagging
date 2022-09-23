6baa10e85c6c322685b02223840ee20d97b4b7bc
Alex Reid <areid@netbuilder.com>
2022-09-23 14:43:12 +0100

    Updates

--- src/helloWorld.tsx
+++
@@ -1,9 +0,0 @@
-import { LDFlagSet } from 'launchdarkly-js-sdk-common';
-import { withLDConsumer } from 'launchdarkly-react-client-sdk';
-
-// Pass the flags prop and return an element based on the state of the feature flag key
-const HelloWorld = (flags: LDFlagSet) => {
-    return flags.displayReactLogo ? <div>Flag on</div> : <div>Flag off</div>;
-};
-
-export default withLDConsumer()(HelloWorld);
